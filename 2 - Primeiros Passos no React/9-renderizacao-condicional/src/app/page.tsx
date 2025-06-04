737d7d6fa7a2cf3bdd5d411214459f4e936146eb
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2025-06-04 20:01:11 -0300

    estudos

---
+++ 2 - Primeiros Passos no React/9-renderizacao-condicional/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Card } from "@/components/Card"
+
+const Page = () => {
+  return (
+    <div className="p-4">
+
+      <Card
+        phrase="Frase de efeito2"
+      />
+
+    </div>
+  )
+}
+
+export default Page
